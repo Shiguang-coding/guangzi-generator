9043b1746d88470bfa4aa943db2bae4395eeb3de
shiguang <an_shiguang@163.com>
2024-06-17 15:12:11 +0800

    项目初始化 1.自述文件 2.忽略文件 3.demo文件 4.basic模块

---
+++ yuzi-generator-basic/src/main/java/com/shiguang/Main.java
@@ -0,0 +1,10 @@
+package com.shiguang;
+
+/**
+ * Created By AnShiGuang329 On $DATE $TIME
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
